9e383b0b68e1a3006d80eaea7b1277e5b4dcc465
Dmitry Popov <kegups@gmail.com>
2021-10-14 18:51:44 +0300

    fixed arenas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function createPlayerObject(name, hp = 100, img = name.toLowerCase(), weapon = ['hand', 'leg', 'head'], attack = 'Fight...'){
-    let newObject = {
+    const newObject = {
         name    : name,
         hp      : hp,
         img     : 'http://reactmarathon-api.herokuapp.com/assets/' + img + '.gif',
@@ -12,9 +12,6 @@ function createPlayerObject(name, hp = 100, img = name.toLowerCase(), weapon = [
     return newObject;
 }
 
-const player1 = createPlayerObject('SUB-ZERO', 80, 'subzero');
-const player2 = createPlayerObject('Scorpion', 50);
-
 function createElemWithClass(className, tag = 'div'){
     const elem = document.createElement(tag);
     elem.className = className;
@@ -23,28 +20,31 @@ function createElemWithClass(className, tag = 'div'){
 }
 
 function createPlayer(className, object){
-    const div = {
-        root: createElemWithClass(className),
-        progressbar: createElemWithClass('progressbar'),
-        character: createElemWithClass('character'),
-        life: createElemWithClass('life'),
-        name: createElemWithClass('name'),
-        img: document.createElement('img'),
-    }
-
-    document.body.querySelector('.arenas').appendChild(div.root);
-
-    div.root.appendChild(div.progressbar);
-    div.root.appendChild(div.character);
-    div.character.appendChild(div.img);
-    div.progressbar.appendChild(div.life);
-    div.progressbar.appendChild(div.name);
-
-    div.life.style.width = object.hp + "%";
-    div.name.innerText = object.name;
-    div.img.src = object.img;
     
+    const   root = createElemWithClass(className),
+            progressbar = createElemWithClass('progressbar'),
+            character = createElemWithClass('character'),
+            life = createElemWithClass('life'),
+            name = createElemWithClass('name'),
+            img = document.createElement('img');
+
+    root.appendChild(progressbar);
+    root.appendChild(character);
+    character.appendChild(img);
+    progressbar.appendChild(life);
+    progressbar.appendChild(name);
+
+    life.style.width = object.hp + "%";
+    name.innerText = object.name;
+    img.src = object.img;
+    
+    arenas.appendChild(root);
 }
+
+const arenas = document.body.querySelector('.arenas');
+
+const player1 = createPlayerObject('SUB-ZERO', 80, 'subzero');
+const player2 = createPlayerObject('Scorpion', 50);
 
 createPlayer('player1', player1);
 createPlayer('player2', player2);
